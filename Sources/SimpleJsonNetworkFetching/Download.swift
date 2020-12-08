947d733b1f44a3254cf0a4939f47e093843b5f9c
carl.hung <carl.hung@yahoo.com>
2020-12-08 21:30:25 +0800

    added reference of download class

--- Sources/SimpleJsonNetworkFetching/Download.swift
+++ Sources/SimpleJsonNetworkFetching/Download.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// // https://stackoverflow.com/questions/30543806/get-progress-from-datataskwithurl-in-swift/45290601
 protocol DownloadTask {
 
 //   var completionHandler: ResultType<Data>.Completion? { get set }
