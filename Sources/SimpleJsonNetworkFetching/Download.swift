ded6371a109148ee2441728c2053be12f8e30a64
carl.hung <carl.hung@yahoo.com>
2020-12-09 01:02:14 +0800

    added supporting linux

--- Sources/SimpleJsonNetworkFetching/Download.swift
+++ Sources/SimpleJsonNetworkFetching/Download.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+#if os(Linux)
+    import FoundationNetworking
+#endif
 
 // // https://stackoverflow.com/questions/30543806/get-progress-from-datataskwithurl-in-swift/45290601
 protocol DownloadTask {
