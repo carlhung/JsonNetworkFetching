4a25610ce35a09fff53e712bf92dcb7308b20cd1
carlhung <carl.hung@yahoo.com>
2021-08-07 16:45:20 +0100

    added init for url which can encode string with space

--- Sources/SimpleJsonNetworkFetching/HTTPMethod.swift
+++ Sources/SimpleJsonNetworkFetching/HTTPMethod.swift
@@ -4,6 +4,10 @@
 //
 //  Created by carlhung on 3/12/2020.
 //
+import Foundation
+#if os(Linux)
+    import FoundationNetworking
+#endif
 
 public protocol Method {
     associatedtype EncodableBody: Encodable
@@ -49,3 +53,36 @@ public extension Method {
 /// Stump struct for being used by `get`
 public struct Stump: Encodable {}
 public typealias MethodGet = HTTPMethod<Stump>
+
+
+private func urlConstructer(scheme: String, host: String, path: String, queryItems: [(key: String, value: String)]) -> URL? {
+    let queryItemArray = queryItems.map { URLQueryItem(name: $0.key, value: $0.value) }
+    let url = URLComponents(scheme: scheme, host: host, path: path, queryItems: queryItemArray).url
+    return url
+}
+
+private extension URLComponents {
+    init(scheme: String,// = "https",
+         host: String,// = "www.google.com",
+         path: String,// = "/search",
+         queryItems: [URLQueryItem]) { // URLQueryItem(name: "q", value: "Formula One")
+        self.init()
+        self.scheme = scheme
+        self.host = host
+        self.path = path
+        self.queryItems = queryItems
+    }
+}
+
+public extension URL {
+    init?(scheme: String,// = "https",
+        host: String,// = "www.google.com",
+        path: String,// = "/search",
+        queryItems:  [(key: String, value: String)]) { // URLQueryItem(name: "q", value: "Formula One")
+        let urlStr = urlConstructer(scheme: scheme, host: host, path: path, queryItems: queryItems)?.absoluteString
+        guard let urlStr = urlStr else {
+            return nil
+        }
+        self.init(string: urlStr)
+    }
+}
